7899bbb656465168d0d97fbca7f79eababe83ff1
darnokkeson <kooonrad.noise@gmail.com>
2025-10-17 20:41:26 +0200

    new exercise

---
+++ GuessTheNumber/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Game();
+        }
+
+        // guess the number - GAME
+        static string Game()
+        {
+
+        }
+
+    }
+}
