e46d5ac4043c09b596ec7021e3be33a40e246c65
darnokkeson <kooonrad.noise@gmail.com>
2025-10-18 17:11:14 +0200

    draft of 'guess the number' game

--- GuessTheNumber/ConsoleApp1/Program.cs
+++ GuessTheNumber/ConsoleApp1/Program.cs
@@ -4,14 +4,64 @@
     {
         static void Main(string[] args)
         {
+            Welcome();
             Game();
         }
 
         // guess the number - GAME
-        static string Game()
+        static void Game()
         {
+            Random random = new Random();
+            int randomNumber = random.Next(1, 100);
+            int userNumber;
+            string userInput;
 
+            Console.WriteLine(randomNumber);
+
+            // I have to make this loop like as a seperated function with
+            // return of userNumber
+            while (true)
+            {
+                Console.WriteLine("Enter a number: ");
+                userInput = Console.ReadLine();
+
+                if (int.TryParse(userInput, out userNumber))
+                {
+                    Console.WriteLine($"You entered a number: {userNumber}");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("That's not a valid number!");
+                }
+            }
+
+
+            while (true)
+            {
+
+                // I have to put here function with return of userNumber
+
+                if (randomNumber == userNumber)
+                {
+                    Console.WriteLine($"is exactly {randomNumber}");
+                    break;
+                }
+                else if (randomNumber > userNumber)
+                {
+                    Console.WriteLine("Too low");
+                }
+                else
+                {
+                    Console.WriteLine("Too High");
+                }
+            }
         }
 
+        static void Welcome()
+        {
+            Console.WriteLine("We’re gonna play 'Guess The Number' game.");
+            Console.WriteLine("Guess the number from scope 1 - 100.");
+        }
     }
 }
