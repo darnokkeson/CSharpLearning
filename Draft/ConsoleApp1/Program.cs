e960b4ae7f3031ee683d56ea1496e9411b3100ad
darnokkeson <kooonrad.noise@gmail.com>
2025-10-20 15:59:21 +0200

    create draft for check modules

---
+++ Draft/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+
+        // DRAFT
+        static void Main(string[] args)
+        {
+            TestProgram(3, 4);
+        }
+
+        static void TestProgram(int floor, int power)
+        {
+            int result = 1;
+
+            for (int i = 0; i < power; i++)
+            {
+                result = result * floor;
+
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
