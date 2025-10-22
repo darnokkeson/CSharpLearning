a91da8dbdd29eae8e6e7f7e44744a08f338f37d3
darnokkeson <kooonrad.noise@gmail.com>
2025-10-22 20:30:22 +0200

    Interfaces explain

---
+++ Interfaces/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Door leftDoor = new Door();
+
+            leftDoor.Interact();
+        }
+
+        interface IInteractable
+        {
+            void Interact();
+        }
+
+        class Door : IInteractable
+        {
+            public void Interact()
+            {
+                Console.WriteLine("Door open.");
+            }
+        }
+    }
+}
