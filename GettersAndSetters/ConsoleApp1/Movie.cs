de20b5b9614b63af7d5a77fcfb68d4b1b28ee4d2
darnokkeson <kooonrad.noise@gmail.com>
2025-10-22 16:54:10 +0200

    get and set

---
+++ GettersAndSetters/ConsoleApp1/Movie.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Movie
+    {
+        public string title;
+        public string director;
+        private string rating; // get and sett allow read and write private
+
+        public Movie(string aTitle, string aDirector, string aRating)
+        {
+            title = aTitle;
+            director = aDirector;
+            Rating = aRating; // have to call to get set function
+        }
+
+        public string Rating
+        {
+            get { return rating; }
+            set
+            {
+                if (value == "5")
+                {
+                    rating = value;
+                }
+                else
+                {
+                    rating = "works";
+                }
+            }
+        }
+    }
+}
---
+++ GettersAndSetters/ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿ namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Movie movie1 = new Movie("1", "2", "4");
+
+            Console.WriteLine(movie1.Rating);
+        }
+    }
+}
