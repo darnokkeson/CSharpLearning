080ee5c644605f9b885778ede48780e4d840f621
darnokkeson <kooonrad.noise@gmail.com>
2025-10-23 15:40:29 +0200

    Abstract Classes trining

---
+++ AbstractClasses/ConsoleApp1/AAnimal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public abstract class AAnimal
+    {
+        // Abstract method - subclasses MUST implement this
+        public abstract void MakeSound();
+
+        // Regular method - subclasses can use or ovveride
+        public void Eat()
+        {
+            Console.WriteLine("This animal is eating.");
+        }
+    }
+}
---
+++ AbstractClasses/ConsoleApp1/Dog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Dog : AAnimal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("woof");
+        }
+    }
+}
